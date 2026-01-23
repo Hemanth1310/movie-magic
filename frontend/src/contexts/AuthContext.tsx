084d94a001ad42c5be87fe94a265d991e4ccfa8c
Hemanth1310 <hemanth13k@gmail.com>
2026-01-23 15:07:54 +0100

    frontend-user-auth-context

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,67 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import type { userData } from '../types'
+import api from '../utils/config/axiosConfig';
+import axios from 'axios';
+
+type AuthContextType = {
+  userData: userData | null,
+  handleUserData: (userData:userData)=>void,
+  isAuthLoading:boolean
+}
+
+type AuthConextProviderType = {
+  children: React.ReactNode;
+};
+
+const defaultUser = {
+  userData:null,
+  handleUserData:()=>{},
+  isAuthLoading:false
+}
+
+export const AuthContext = createContext<AuthContextType>(defaultUser)
+
+
+export const AuthContextProvider  = ({children}: AuthConextProviderType) => {
+
+  const [userData,setUserData] = useState<userData | null>(null)
+  const [isAuthLoading, setIsAuthLoading] = useState<boolean>(true)
+
+  const handleUserData = (data:userData | null)=>{
+       setUserData(data)
+  }
+
+  useEffect(()=>{
+   const userCheck=async()=>{
+     const token = localStorage.getItem('mmtoken')
+      if(!token){
+        setIsAuthLoading(false)
+        return
+      }
+      try{
+        const {data} =await api.get('/api/auth/userDetails')
+        setUserData(data)
+        setIsAuthLoading(false)
+      }catch(err){
+        if(axios.isAxiosError(err)){
+          console.log(err.response?.data?.message)
+        }else{
+          console.log("Unexpected Error: Session verification failed")
+        }
+        setIsAuthLoading(false)
+        localStorage.removeItem("mmtoken")
+        handleUserData(null)
+      }
+   }
+   userCheck();
+  },[])
+  return (
+   <AuthContext.Provider value={{userData,isAuthLoading,handleUserData}}>
+      {children}
+   </AuthContext.Provider>
+  )
+}
+
+export const useAuth = ()=>{
+  return useContext(AuthContext)
+}
