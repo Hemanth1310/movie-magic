39b049bbb2e87670622e466eb7a34664e6bdde45
Hemanth1310 <hemanth13k@gmail.com>
2026-01-23 15:05:50 +0100

    frontend-app-main-setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./views/Home";
+import Header from "./components/ui/Header";
+import ProtecterRoutes from "./middleware/auth/ProtecterRoutes";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AuthContextProvider>
+      <div className="min-h-screen bg-gray-50">
+        <BrowserRouter>
+          <div className="w-screen min-h-screen flex flex-col">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route element={<ProtecterRoutes/>}>
+                
+              </Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </AuthContextProvider>
+  );
 }
 
-export default App
+export default App;
