b3e1b340af8265cf6b46ff3280ed27da39907044
Hemanth1310 <hemanth13k@gmail.com>
2026-01-23 13:25:50 +0100

    backend-movie-table-images-update

---
+++ backend/prisma/migrations/20260123115233_imagepath_name_added/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Movie" ADD COLUMN     "imagePath" TEXT NOT NULL DEFAULT '';
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,30 @@
 import express from "express";
 import authRouter from './authRoutes'
+import path from "path";
 const app = express()
 
 app.use('/api/auth',authRouter)
+
+const publicPath = path.join(__dirname, '..',"public")
+
+app.use('/images',express.static('public'))
+
+app.get('/images/:filename',(req,res)=>{
+    const filename = req.params.filename
+    const imagePath = path.join(publicPath,filename)
+
+    if(!imagePath){
+        res.status(400).json({
+            message:"Invalid method."
+        })
+    }
+
+    res.sendFile(imagePath,(err)=>{
+        res.status(405).json({
+            message:'Image not found'
+        })
+    })
+})
 
 app.listen(3008,()=>{
     console.log("Listening at 3008")
