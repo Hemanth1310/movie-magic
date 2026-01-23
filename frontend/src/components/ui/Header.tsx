f9710a518581eab491ecdc704161197332678306
Hemanth1310 <hemanth13k@gmail.com>
2026-01-23 15:21:58 +0100

    frontend-header-setup

---
+++ frontend/src/components/ui/Header.tsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <div className="flex-1 w-screen h-20 bg-gray-800 flex items-center justify-center fixed top-0 left-0 z-100 shadow-md">
+      <div className="container ">
+        <div className="font-bold text-neutral-100 text-2xl">MovieMagic</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
