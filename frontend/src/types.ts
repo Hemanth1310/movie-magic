648319c9008c82b42e380227135c3f7577129d36
Hemanth1310 <hemanth13k@gmail.com>
2026-01-24 16:32:52 +0100

    frontend-type-mod

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,8 +1,18 @@
-export type userData={
+export type UserData={
     id: string;
     email: string;
     firstName: string;
     lastName: string;
     imagePath: string;
     isVerified: boolean;
+}
+
+type genre =  'PENDING_PAYMENT' | 'CONFIRMED' | 'CANCELLED' | 'FAILED'
+
+export type Movies={
+    id: string;
+    title: string;
+    description: string | null;
+    genre: genre;
+    duration: number;
 }
