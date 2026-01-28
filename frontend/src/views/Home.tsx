e3de558ff308094c11fdb9de4db453f70602bcfb
Hemanth1310 <hemanth13k@gmail.com>
2026-01-28 15:50:38 +0100

    frontend-homepage-mod

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,7 +1,17 @@
+import Banner from "../components/ui/Banner"
+import useMoviesDataProvider from "../utils/moviesDataProvider"
 
 const Home = () => {
+  const { allMoviesData,
+        feturedMoviesData,
+        moviesList,
+        isLoading,
+        isError,
+        refetch} = useMoviesDataProvider()
   return (
-    <div>Home</div>
+    <div className="container flex flex-col items-center justify-between">
+      <Banner featuredMoviesData={feturedMoviesData?feturedMoviesData : []}/>
+    </div>
   )
 }
 
