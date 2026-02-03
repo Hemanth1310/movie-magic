5cfec87dac28c09517658d309b11e1e458fa2572
Hemanth1310 <hemanth13k@gmail.com>
2026-02-03 17:22:45 +0100

    frontend-error-handling-on-data-fetch

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,18 +1,36 @@
-import Banner from "../components/ui/Banner"
-import useMoviesDataProvider from "../utils/moviesDataProvider"
+import Banner from "../components/ui/Banner";
+import useMoviesDataProvider from "../utils/moviesDataProvider";
 
 const Home = () => {
-  const { allMoviesData,
-        feturedMoviesData,
-        moviesList,
-        isLoading,
-        isError,
-        refetch} = useMoviesDataProvider()
+  const {
+    allMoviesData,
+    feturedMoviesData,
+    moviesList,
+    isLoading,
+    isError,
+    refetch,
+  } = useMoviesDataProvider();
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex flex-col gap-5 font-mono italic text-gray-500 items-center justify-center text-3xl">
+        "Page is loading . please wait"
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="w-full h-screen flex flex-col gap-5 font-mono italic text-gray-500 items-center justify-center text-3xl">
+        "Failed to load the page"
+        <span onMouseDown={() => refetch()}>Refetch</span>
+      </div>
+    );
+  }
   return (
     <div className="container flex flex-col items-center justify-between">
-      <Banner featuredMoviesData={feturedMoviesData?feturedMoviesData : []}/>
+      <Banner featuredMoviesData={feturedMoviesData ? feturedMoviesData : []} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
