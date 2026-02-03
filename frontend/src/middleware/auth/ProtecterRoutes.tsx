9f4cf96ac6d3a5965f31a27de0c9d56eb88f2d82
Hemanth1310 <hemanth13k@gmail.com>
2026-02-03 17:04:58 +0100

    frontend-protected-rotes-definition

--- frontend/src/middleware/auth/ProtecterRoutes.tsx
+++ frontend/src/middleware/auth/ProtecterRoutes.tsx
@@ -1,5 +1,21 @@
+import { Navigate, Outlet } from "react-router"
+import { useAuth } from "../../contexts/AuthContext"
 
 const ProtecterRoutes = () => {
+  const {userData,isAuthLoading} = useAuth()
+
+  if(isAuthLoading){
+    return <div className="w-full h-screen flex flex-col gap-5 font-mono italic text-gray-500 items-center justify-center text-3xl">
+        "Page is loading . please wait"
+      </div>
+  }
+
+  if(userData){
+    return <Outlet/>
+  }else{
+    return <Navigate to='/' />
+  }
+
   return (
     <div>ProtecterRoutes</div>
   )
