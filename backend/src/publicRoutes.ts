a4a833f9471fbafaef1cbc7c7a97ffda4ce631d9
Hemanth1310 <hemanth13k@gmail.com>
2026-02-04 17:39:40 +0100

    backend-screening-details-endpoint

--- backend/src/publicRoutes.ts
+++ backend/src/publicRoutes.ts
@@ -10,17 +10,52 @@ router.get("/all-movies", async (req, res) => {
     const movies = await prisma.movie.findMany();
 
     if (movies.length <= 0) {
-      res.status(404).json({ message: "Movies data not found" });
+      return res.status(404).json({ message: "Movies data not found" });
     }
 
-    res.json({
+    return res.json({
       payload: {
         movies: movies,
       },
     });
   } catch (err) {
-    res.status(500).json({ message: err });
+    return res.status(500).json({ message: err });
   }
 });
+
+router.get('/screenings/:movieId',async(req,res)=>{
+    const {movieId} = req.params
+
+    try{
+        const screeningDetails = await prisma.showtime.findMany({
+            where:{movieId:movieId},
+            include:{
+                movie:true,
+                screen:{
+                    include:{
+                        theater:true
+                    }
+                }
+            }
+        }) 
+
+        if(screeningDetails.length<=0){
+            return res.status(404).json({
+            message:'Screening detials for requested movie not available'
+        })
+        }
+
+        return res.json({
+            message:'Screeing details',
+            payload:{
+                screeningDetails
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            message:'Internal Server Error'
+        })
+    }
+})
 
 export default router
