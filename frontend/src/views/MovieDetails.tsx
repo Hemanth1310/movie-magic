c043353757268792e6d5a92492ebd1b3116c7cb8
Hemanth1310 <hemanth13k@gmail.com>
2026-02-04 17:43:16 +0100

    frontend-movie-details-page-skeleton

---
+++ frontend/src/views/MovieDetails.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import { useScreeingDetails } from '../utils/hooks/dataQueryHooks'
+import ErrorFallback from '../components/error/ErrorFallback'
+import Spinner from '../components/ui/Spinner'
+import getImageUrl from '../utils/getImageUrl'
+
+const MovieDetails = () => {
+  const {movieId} = useParams()
+  const {data:screeningDetails, isError, isLoading, refetch} = useScreeingDetails(movieId!)
+
+  
+  if (!screeningDetails) {
+    return (
+      <div className="w-full h-screen flex font-mono italic text-gray-500 items-center justify-center text-3xl">
+        Failed to fetch details
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex font-mono italic text-gray-500 items-center justify-center text-3xl">
+        "Page is loading . please wait"
+        <Spinner/>
+      </div>
+    );
+  }
+  if (isError) {
+   return (
+     <ErrorFallback onRetry={refetch}/>
+    );
+  }
+
+  return (
+    <div className="container flex flex-col items-center justify-between">
+      <div className='w-full flex p-10 bg-gray-200 text-2xl font-bold'>
+        Movie Details
+      </div>
+     <div className='w-full flex gap-10 p-10 md:mt-10'>
+      <div>
+          <div className='flex flex-col gap-5'>
+            <img className="h-120 w-80" src={getImageUrl(screeningDetails[0].movie.imagePath)}/>
+            <div className=' ml-5'>
+              <h1 className='text-2xl'>{screeningDetails[0].movie.title}</h1>
+              <p>Genre: {screeningDetails[0].movie.genre}</p>
+              <p>Duration: {screeningDetails[0].movie.duration}Mins</p>
+            </div>
+          </div>
+      </div>
+      <div>
+        {screeningDetails.map(item=><div key={item.id}>
+          {item.screen.theater.name}{item.screen.name}
+        </div>)}
+      </div>
+     </div>
+    </div>
+    
+  )
+}
+
+export default MovieDetails
