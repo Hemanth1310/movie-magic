92f96b67f6805c725ac279a97529a9212c63c64c
Hemanth1310 <hemanth13k@gmail.com>
2026-02-04 17:41:43 +0100

    frontend-loading-spinner-component

---
+++ frontend/src/components/ui/Spinner.tsx
@@ -0,0 +1,11 @@
+const Spinner = () => {
+  return (
+    <div
+      className="w-16 h-16 border-4 border-blue-500
+                        border-t-transparent rounded-full 
+                        animate-spin"
+    ></div>
+  );
+};
+
+export default Spinner;
