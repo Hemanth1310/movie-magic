4a0a13464c63e8bcc718c9105e5078d41f026c65
Hemanth1310 <hemanth13k@gmail.com>
2026-02-04 17:42:21 +0100

    frontend-home-navigation-to-movie-details

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router";
 import Banner from "../components/ui/Banner";
+import Spinner from "../components/ui/Spinner";
 import getImageUrl from "../utils/getImageUrl";
 import useMoviesDataProvider from "../utils/moviesDataProvider";
 
 const Home = () => {
+  const navigate= useNavigate()
   const {
     allMoviesData,
     feturedMoviesData,
-    moviesList,
     isLoading,
     isError,
     refetch,
@@ -16,6 +18,7 @@ const Home = () => {
     return (
       <div className="w-full h-screen flex flex-col gap-5 font-mono italic text-gray-500 items-center justify-center text-3xl">
         "Page is loading . please wait"
+        <Spinner/>
       </div>
     );
   }
@@ -27,6 +30,10 @@ const Home = () => {
       </div>
     );
   }
+
+  const handleMovieDetails = (id:string)=>{
+      navigate(`/movie-details/${id}`)
+  }
   return (
     <div className="container flex flex-col items-center justify-between">
       <Banner featuredMoviesData={feturedMoviesData ? feturedMoviesData : []} />
@@ -34,7 +41,7 @@ const Home = () => {
         <h2 className="text-2xl md:text-4xl">Currently screening in cinemas</h2>
         <div className="flex flex-wrap items-center justify-between mt-10">
         {allMoviesData?.map((movie)=>
-        <div className="flex flex-col">
+        <div key={movie.id} className="flex flex-col" onClick={()=>handleMovieDetails(movie.id)}>
           <img className="h-100 w-70" src={getImageUrl(movie.imagePath)}/>
           <h3>{movie.title}</h3>
           <p>{movie.genre}</p>
