f0eae5b2767b94ebd09763e6d922fb7ed4dcf609
Hemanth1310 <hemanth13k@gmail.com>
2026-02-05 18:03:05 +0100

    frontend-screening-details-formatting

--- frontend/src/components/ui/ScreeningDetails.tsx
+++ frontend/src/components/ui/ScreeningDetails.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import type { ScreeningDetail } from '../../types'
+import React from "react";
+import screeningDetailsProvider from "../../utils/screeningDetailsProvider";
+import type { ScreeningDetail } from "../../types";
 
 type Props = {
-    item: ScreeningDetail
-}
+  screeningDetails: ScreeningDetail[];
+};
 
-const ScreeningDetails = ({item}: Props) => {
-
+const ScreeningDetails = ({ screeningDetails }: Props) => {
+  const formattedScreeningDetails = screeningDetailsProvider(screeningDetails);
 
   return (
-   <div className='w-full p-5 border-2 border-gray-100 ' key={item.id}>
-          <h3 className='text-2xl' >{item.screen.theater.name}</h3>
-          <p>{item.screen.theater.location}</p>
-          <p> {item.screen.name}</p>
-          <p>{new Date(item.startTime).toLocaleDateString()}</p>
-          {new Date(item.startTime).getHours().toString().padStart(2,'0')}: {new Date(item.startTime).getMinutes()}
+    <div className="w-full">
+      {Object.keys(formattedScreeningDetails).map((dateKey) => (
+        <div key={dateKey}>
+          <h2 className="text-2xl pb-10">{dateKey}</h2>
+          <div className=" grid grid-cols-3 gap-5 pb-10">
+            {formattedScreeningDetails[dateKey].map((item) => (
+              <div
+                className="flex flex-col gap-1 p-5 border-2 border-gray-200"
+                key={item.id}
+              >
+                <h3 className="text-2xl">{item.screen.theater.name}</h3>
+                <p>@{item.screen.theater.location}</p>
+                <p className="font-bold">{item.screen.name}</p>
+                <span className="border-2 border-gray-200">
+                  {new Date(item.startTime)
+                    .getHours()
+                    .toString()
+                    .padStart(2, "0")}
+                  :{new Date(item.startTime).getMinutes()}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default ScreeningDetails
+export default ScreeningDetails;
