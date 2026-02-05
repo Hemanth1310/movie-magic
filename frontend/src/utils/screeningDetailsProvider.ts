3c64e5a6cf155519d212de00c987aac4e2646b15
Hemanth1310 <hemanth13k@gmail.com>
2026-02-05 17:42:44 +0100

    frontend-screenDetails-formatter-by-date

---
+++ frontend/src/utils/screeningDetailsProvider.ts
@@ -0,0 +1,20 @@
+import type { ScreeningDetail } from "../types";
+
+
+const screeningDetailsProvider = (screeningDetails:ScreeningDetail[])=>{
+    const formattedScreeningDetails: Record<string, ScreeningDetail[]>  ={}
+
+    for(let i=0;i<screeningDetails.length;i++){
+        const date = new Date(screeningDetails[i].startTime).toLocaleDateString()
+        if(!formattedScreeningDetails[date]){
+            formattedScreeningDetails[date]=[]
+        }
+       formattedScreeningDetails[date].push(screeningDetails[i])
+       console.log(screeningDetails[i])
+    }
+
+    return formattedScreeningDetails
+
+}
+
+export default screeningDetailsProvider
