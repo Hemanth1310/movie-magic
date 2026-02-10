735c87bdd696d816097aac37d9decffe5f279a38
Hemanth1310 <hemanth13k@gmail.com>
2026-02-10 15:38:25 +0100

    frontend-conmopnents-for-login-and-register

---
+++ frontend/src/components/ui/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+    handleToggle:(moveTo:string)=>void
+}
+
+const Login = ({handleToggle}: Props) => {
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/components/ui/Register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {
+    handleToggle:(moveTo:string)=>void
+}
+const Register = ({handleToggle}: Props) => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
