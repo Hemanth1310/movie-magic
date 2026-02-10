53180d11bddc3f58e60ae4c0b452844fdfc7cf2a
Hemanth1310 <hemanth13k@gmail.com>
2026-02-10 15:38:50 +0100

    frontend-header-modal-config

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
 import { useAuth } from "../../contexts/AuthContext"
+import AuthLayout from "./AuthLayout"
 
 
 const Header = () => {
   const {userData} = useAuth()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  
+  const closeModal=() =>{
+    setIsModalOpen(false)
+  }
+  const openModal =()=>{
+    setIsModalOpen(true)
+  }
+
   return (
     <div className="flex-1 w-screen h-20 bg-brand-primary flex items-center justify-center fixed top-0 left-0 z-100 shadow-md">
       <div className="container w-full flex items-center justify-between">
@@ -16,11 +27,15 @@ const Header = () => {
           {userData?  <span className="material-symbols-outlined acc-icon">
             account_circle
           </span>:
-          <button className="font-bold text-neutral-100 text-xl">Sign In</button>
+          <button className="font-bold text-neutral-100 text-xl" onClick={openModal}>Sign In</button>
           }
           
         </div>
       </div>
+      <AuthLayout
+      isModalOpen={isModalOpen}
+      closeModal={closeModal}
+      />
     </div>
   )
 }
